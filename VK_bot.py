bba715b7b10f02c60d1773948cf3aff19f1c4cf6
IaponaMother <58094880+IaponaMother@users.noreply.github.com>
2021-03-29 23:40:02 +0700

    Update VK_bot.py

--- VK_bot.py
+++ VK_bot.py
@@ -1,8 +1,8 @@
 from tokens import token, PyOwm
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
-import requests
-import bs4
+import requests, httplib2
+from bs4 import BeautifulSoup, SoupStrainer
 from pyowm import OWM
 from pyowm.utils.config import get_default_config
 from pyowm.commons.exceptions import NotFoundError
@@ -11,7 +11,6 @@ vk_session = vk_api.VkApi(token=token)
 session_api = vk_session.get_api()
 longpoll = VkLongPoll(vk_session)
 vk = vk_session.get_api()
-
 
 def weather(place):
     config_dict = get_default_config()
@@ -29,9 +28,35 @@ def weather(place):
         else:
             return f"В городе {place.title()} сейчас {w.detailed_status} \n Температура: {round(temp)}{degree_sign}С"
 
-
     except NotFoundError:
         return f'Не найден город: {place.title()}'
+
+
+
+def get_fanfiction(fandom):
+    url = "https://ficbook.net/fanfiction/%20/" + fandom.replace(" ", "_")
+    fanfictions = []
+    names = []
+    http = httplib2.Http()
+    status, response = http.request(url)
+    for link in BeautifulSoup(response, 'html.parser', parse_only=SoupStrainer('a')):
+        if link.has_attr('href'):
+            if "/readfic/" in link['href'] and not "premium" in link['href']:
+                fanfictions.append("http://ficbook.net" + link['href'])
+    fanfictions = fanfictions[:3]
+
+    for f in fanfictions:
+        r = requests.get(f)
+        soup = BeautifulSoup(r.text, 'lxml')
+        name = soup.find_all('h1')
+        names.append([elem.string for elem in name])
+    it = 0
+    result = []
+    for i in names:
+        for j in i:
+            result.append(fanfictions[it] + " --> " + j + "\n")
+            it += 1
+    return result
 
 
 def _clean_all_tag_from_str(string_line):
@@ -46,21 +71,19 @@ def _clean_all_tag_from_str(string_line):
         else:
             if i == ">":
                 not_skip = True
-
     return result
 
 
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
         text = event.text.lower()
-        if text == 'привет' or text == 'хай':
-            if event.from_user:
 
+        if text == 'привет' or 'хай':
+            if event.from_user:
                 request = requests.get("https://vk.com/id" + str(event.user_id))
-                bs = bs4.BeautifulSoup(request.text, "html.parser")
+                bs = BeautifulSoup(request.text, "html.parser")
                 user_name = _clean_all_tag_from_str(bs.findAll("title")[0])
                 your_name = user_name.split()[0]
-
                 vk.messages.send(user_id=event.user_id, message=f'Привет-привет, {your_name}!', random_id=0)
             elif event.from_chat:
                 vk.messages.send(chat_id=event.chat_id, message='Привет-Привет, ребята!', random_id=0)
@@ -71,8 +94,17 @@ for event in longpoll.listen():
             elif event.from_chat:
                 vk.messages.send(chat_id=event.chat_id, message=weather(text[16:]), random_id=0)
 
+        elif text[:11] == 'фанфики по ':
+            if event.from_user:
+                vk.messages.send(user_id=event.user_id, message=get_fanfiction(text[11:].lower()), random_id=0)
+            elif event.from_chat:
+                vk.messages.send(user_id=event.chat_id, message=get_fanfiction(text[11:].lower()), random_id=0)
+
+        elif text == "пока" or "бай":
+            if event.from_user:
+                vk.messages.send(user_id=event.user_id, message="Пока-пока!", random_id=0)
+            elif event.from_chat:
+                vk.messages.send(user_id=event.chat_id, message="Пока-пока, ребята!", random_id=0)
+
         else:
             pass
-
-
-
