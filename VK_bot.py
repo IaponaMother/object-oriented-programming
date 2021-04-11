1c338dbfacf8865a134e2202f673b00d95e8bdf2
IaponaMother <58094880+IaponaMother@users.noreply.github.com>
2021-04-11 10:09:04 +0700

    Update VK_bot.py

--- VK_bot.py
+++ VK_bot.py
@@ -6,18 +6,20 @@ from bs4 import BeautifulSoup, SoupStrainer
 from pyowm import OWM
 from pyowm.utils.config import get_default_config
 from pyowm.commons.exceptions import NotFoundError
+import sqlite3
+
 
 vk_session = vk_api.VkApi(token=token)
 session_api = vk_session.get_api()
 longpoll = VkLongPoll(vk_session)
 vk = vk_session.get_api()
 
+
 def weather(place):
     config_dict = get_default_config()
     config_dict['language'] = 'ru'
     owm = OWM(PyOwm)
     mgr = owm.weather_manager()
-
     try:
         observation = mgr.weather_at_place(place)
         w = observation.weather
@@ -73,11 +75,18 @@ def _clean_all_tag_from_str(string_line):
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
         text = event.text.lower()
-
         request = requests.get("https://vk.com/id" + str(event.user_id))
         bs = BeautifulSoup(request.text, "html.parser")
         user_name = _clean_all_tag_from_str(bs.findAll("title")[0])
-        your_name = user_name.split()[0]
+        your_name = str(user_name.split()[0])
+        your_lastname = str(user_name.split()[1])
+        your_id = int(event.user_id)
+        with sqlite3.connect('database.db') as db:
+            cursor = db.cursor()
+            add_unit = ("INSERT INTO Users " "(id, name, lastname) " "VALUES ('%(id)s', '%(name)s', '%(lastname)s')" % {
+                'id': your_id, 'name': your_name, 'lastname': your_lastname})
+            cursor.execute(add_unit)
+        
 
         if text[:16] == 'погода в городе ':
             if event.from_user:
